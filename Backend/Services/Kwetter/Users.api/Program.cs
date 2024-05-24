7f8a05d7c8af61a252db773747c806ab654d4e47
Gino Stock <stockje@outlook.com>
2024-05-24 13:00:22 +0200

    commented out rabbit in program

--- Backend/Services/Kwetter/Users.api/Program.cs
+++ Backend/Services/Kwetter/Users.api/Program.cs
@@ -45,19 +45,19 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
 });
 
-builder.Services.AddMassTransit(busConfigurator =>
-{
-    busConfigurator.SetKebabCaseEndpointNameFormatter();
-    busConfigurator.UsingRabbitMq((context, configurator) => {
-        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
-        {
-            h.Username(builder.Configuration["MessageBroker:Username"]);
-            h.Password(builder.Configuration["MessageBroker:Password"]);
-        });
+//builder.Services.AddMassTransit(busConfigurator =>
+//{
+//    busConfigurator.SetKebabCaseEndpointNameFormatter();
+//    busConfigurator.UsingRabbitMq((context, configurator) => {
+//        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
+//        {
+//            h.Username(builder.Configuration["MessageBroker:Username"]);
+//            h.Password(builder.Configuration["MessageBroker:Password"]);
+//        });
 
-        configurator.ConfigureEndpoints(context);
-    });
-});
+//        configurator.ConfigureEndpoints(context);
+//    });
+//});
 
 var app = builder.Build();
 
